7f5518afc9d77591d9f72466033b4943a388b0dd
chriscoulbeck <chriscoulbeck7@gmail.com>
2020-12-15 10:12:12 +1300

    commit for github page

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,26 +9,3 @@ $(document).ready(function () {
 
 const images = document.getElementsByClassName('img');
 let sliderIndex = 0;
-
-
-// Open Modal on Click
-$(document).ready(function() {
-  $('.img').on("click", function() {
-    $('.gallery-modal').css('display', 'flex');
-    let imgUrl = $(this).data('img');
-    $('.modal-image').css('background-image', `url(${imgUrl})`);
-    $('html').css('overflow-y', "hidden");
-  });
-})
-
-// Close Modal on Click
-$(document).ready(function() {
-  $('.icon-cross').on('click', function() {
-    $('.gallery-modal').css('display', 'none');
-    $('html').css('overflow-y', "auto");
-  });
-})
-
-$(document).ready(function() {
-  $('.image-num').text('CHRIST IS KING');
-})
